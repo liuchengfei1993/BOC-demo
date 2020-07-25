a46e3d86ce333094c44ddf57945e109dbf983f8a
lixinyi <466162832@qq.com>
2020-07-25 11:23:23 +0800

    lxy

---
+++ src/page/app-frame.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <appheader></appheader>
+  </div>
+</template>
+<script>
+import appheader from "@/components/header";
+export default {
+  components: { appheader },
+  data() {
+    return {};
+  }
+};
+</script>
+ <style  scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/page/home'
-
 Vue.use(Router)
 
 export default new Router({
   routes: [{
+    path: '/login',
+    name: 'Login',
+    component:resolve =>require(['@/page/login/login'],resolve)
+  },
+   {
     path: '/',
-    name: 'home',
-    component: home
-  }]
+    component:resolve =>require(['@/page/app-frame'],resolve) ,
+    children: [{
+      path: 'home',
+      name: 'Home',
+      component: resolve =>require(['@/page/home/home'],resolve) ,
+    }]}]
 })
